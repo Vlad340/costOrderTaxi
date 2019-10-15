a65baaf7c5e607f4aa002b349fc0f3c12663d28d
Vlad340 <abcd-mail@yandex.ru>
2019-10-15 22:31:20 +0300

    changed 'TripTest' #6

--- src/test/java/TripTest.java
+++ src/test/java/TripTest.java
@@ -5,27 +5,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class TripTest {
 
     @Test
-    void calculationAmount() {
-        {
-            TripService trip = new TripService();
-            long amount = trip.calculationAmount(0)/100;
-            assertEquals(0,amount);
-        }
-        {
+    public void calculationAmountZero() {
+        TripService trip = new TripService();
+        long amount = trip.calculationAmount(0) / 100;
+        assertEquals(0, amount);
+    }
+
+    @Test
+    public void calculationAmountNoBonus() {
             TripService trip = new TripService();
             long amount = trip.calculationAmount(25)/100;
             assertEquals(560,amount);
-        }
-        {
+    }
+
+    @Test
+    public void calculationAmountWithBonus() {
             TripService trip = new TripService();
             long amount = trip.calculationAmount(50)/100;
             assertEquals(1_007,amount);
-        }
-       {
+    }
+
+    @Test
+    public void calculationAmountWithLimitBonus() {
             TripService trip = new TripService();
             long amount = trip.calculationAmount(100)/100;
             assertEquals(1_960,amount);
-        }
-
     }
 }
