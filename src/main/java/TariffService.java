544d509512cfce40814db5895a5e7ba521954376
Vlad340 <abcd-mail@yandex.ru>
2019-10-15 22:48:11 +0300

    changed 'TariffService' (initialization with constructor) #7

--- src/main/java/TariffService.java
+++ src/main/java/TariffService.java
@@ -1,7 +1,15 @@
 public class TariffService {
-    long landingPrice = 60_00L;
-    long tripPrice = 20_00L;
-    long amountToCalculateDiscount = 1_000_00L;
-    int discount = 5;
-    long maximumAmountDiscount = 100_00L;
+    long landingPrice;
+    long tripPrice;
+    long amountToCalculateDiscount;
+    int discount;
+    long maximumAmountDiscount;
+
+    public TariffService() {
+        this.landingPrice = 60_00L;;
+        this.tripPrice = 20_00L;
+        this.amountToCalculateDiscount = 1_000_00L;
+        this.discount = 5;
+        this.maximumAmountDiscount = 100_00L;
+    }
 }
